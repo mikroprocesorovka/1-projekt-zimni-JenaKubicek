54c4593d97c6c5e5c89b9f32f7285d839151802f
JenaKubicek <jena.kubicek@gmail.com>
2022-01-18 19:12:24 +0100

    ADC a nachystany PWM

--- src/main.c
+++ src/main.c
@@ -1,52 +1,69 @@
 #include "stm8s.h"
 #include "milis.h"
-
-/*#include "delay.h"*/
+#include "stm8_hd44780.h"
+#include "spse_stm8.h"
+#include "delay.h"
 #include <stdio.h>
-/*#include "uart1.h"*/
-
 #define _ISOC99_SOURCE
 #define _GNU_SOURCE
 
-#define LED_PORT GPIOC
-#define LED_PIN  GPIO_PIN_5
-#define LED_HIGH   GPIO_WriteHigh(LED_PORT, LED_PIN)
-#define LED_LOW  GPIO_WriteLow(LED_PORT, LED_PIN)
-#define LED_REVERSE GPIO_WriteReverse(LED_PORT, LED_PIN)
+void ADC_init(void){
+// na pinech/vstupech ADC_IN2 (PB2) a ADC_IN3 (PB3) vypneme vstupní buffer
+ADC2_SchmittTriggerConfig(ADC2_SCHMITTTRIG_CHANNEL2,DISABLE);
+// nastavíme clock pro ADC (16MHz / 4 = 4MHz)
+ADC2_PrescalerConfig(ADC2_PRESSEL_FCPU_D4);
+// volíme zarovnání výsledku (typicky vpravo, jen vyjmečně je výhodné vlevo)
+ADC2_AlignConfig(ADC2_ALIGN_RIGHT);
+// nasatvíme multiplexer na některý ze vstupních kanálů
+ADC2_Select_Channel(ADC2_CHANNEL_2);
+// rozběhneme AD převodník
+ADC2_Cmd(ENABLE);
+// počkáme než se AD převodník rozběhne (~7us)
+ADC2_Startup_Wait();
+}
+void TIM2_setup(void){
+    TIM2_DeInit();
+    TIM2_TimeBaseInit(TIM2_PRESCALER_1, 640 - 1);//25kHz    
+    TIM2_OC2Init(TIM2_OCMODE_PWM1, TIM2_OUTPUTSTATE_ENABLE, 1, 
+                TIM2_OCPOLARITY_LOW);
+    TIM2_Cmd(ENABLE);
+    TIM2_OC2PreloadConfig(ENABLE);
 
-#define BTN_PORT GPIOE
-#define BTN_PIN  GPIO_PIN_4
-#define BTN_PUSH (GPIO_ReadInputPin(BTN_PORT, BTN_PIN)==RESET) 
+}
 
 
-void setup(void)
+void init(void)
 {
     CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
-    GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
-    GPIO_Init(BTN_PORT, BTN_PIN, GPIO_MODE_IN_FL_NO_IT);
-
     init_milis();
-    /*init_uart1();*/
+    lcd_init();
+    ADC_init();
+    TIM2_setup();
 }
 
 
 int main(void)
 {
-    uint32_t time = 0;
-
-    setup();
+    init();
+    uint32_t timeA = 0;
+    uint16_t adc_value;
+    char text[32];
+    int16_t teplota;
+    int16_t teplota_1_cast;
+    int16_t teplota_2_cast;
 
     while (1) {
+        if (milis()-timeA>100){
+            timeA=milis();
+            adc_value = ADC_get(ADC2_CHANNEL_2); // do adc_value ulož výsledek převodu vstupu ADC_IN2 (PB2)
+            teplota = ((uint32_t)adc_value*5000 + 512)/1024;
+            teplota_1_cast=teplota/10;//celočíselná část
+            teplota_2_cast=teplota%10;//desetinná část
 
-        if (milis() - time > 333 && BTN_PUSH) {
-            LED_REVERSE; 
-            time = milis();
-            /*printf("%ld\n", time);*/
         }
-
-        /*LED_REVERSE; */
-        /*_delay_ms(333);*/
-        /*printf("Funguje to!!!\n");*/
+        lcd_gotoxy(0, 0);
+        sprintf(text,"Teplota = %2u %1u C",teplota_1_cast,teplota_2_cast);
+        lcd_puts(text);
     }
 }
 

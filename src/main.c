855105ca239752d1d49eda4c49c42837537b0e1a
JenaKubicek <jena.kubicek@gmail.com>
2022-03-01 16:16:48 +0100

    Konec

--- src/main.c
+++ src/main.c
@@ -10,6 +10,10 @@
 #define BTN_PORT GPIOE
 #define BTN_PIN  GPIO_PIN_4
 #define BTN_PUSH (GPIO_ReadInputPin(BTN_PORT, BTN_PIN)==RESET)
+#define FAN_PORT GPIOB 
+#define FAN_PIN GPIO_PIN_7
+#define FAN_OFF  GPIO_WriteHigh(FAN_PORT,FAN_PIN)
+#define FAN_ON GPIO_WriteLow(FAN_PORT,FAN_PIN)
 
 void ADC_init(void){
 ADC2_SchmittTriggerConfig(ADC2_SCHMITTTRIG_CHANNEL2,DISABLE); //PB2
@@ -22,7 +26,7 @@ ADC2_Startup_Wait(); // počkáme než se AD převodník rozběhne (~7us)
 void TIM2_setup(void){
     TIM2_DeInit();
     TIM2_TimeBaseInit(TIM2_PRESCALER_1, 640 - 1);//25kHz    
-    TIM2_OC1Init(TIM2_OCMODE_PWM1, TIM2_OUTPUTSTATE_ENABLE, 400, 
+    TIM2_OC1Init(TIM2_OCMODE_PWM1, TIM2_OUTPUTSTATE_ENABLE, 0, 
                 TIM2_OCPOLARITY_HIGH); // PD4
     TIM2_Cmd(ENABLE);
     TIM2_OC2PreloadConfig(ENABLE);
@@ -31,6 +35,7 @@ void TIM2_setup(void){
 void init(void)
 {
     CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
+    GPIO_Init(FAN_PORT,FAN_PIN,GPIO_MODE_OUT_PP_LOW_SLOW);
     init_milis();
     lcd_init();
     ADC_init();
@@ -43,13 +48,14 @@ int main(void){
     init();
     uint32_t timeA = 0;
     uint16_t timeB = 0;
+    uint16_t dcl = 0;
     uint16_t adc_value;
     char text[32];
     char text2[32];
     uint16_t voltage;
     int16_t teplota_1_cast = 0;
     int16_t teplota_2_cast = 0;
-    uint8_t rychlost;
+    uint8_t rychlost = 0;
 
     while (1) {
         if (milis() - timeA > 333){
@@ -59,49 +65,53 @@ int main(void){
             teplota_1_cast=voltage/10;//celočíselná část
             teplota_2_cast=voltage%10;//desetinná část
             lcd_gotoxy(0, 0);
-            sprintf(text,"Teplota = %2u %1u C",teplota_1_cast,teplota_2_cast);
+            sprintf(text,"Teplota = %2u.%1u C",teplota_1_cast,teplota_2_cast);
             lcd_puts(text);
-            lcd_gotoxy(1,0);
-            sprintf(text2,"Rychlost = %3u %",rychlost);
+            lcd_gotoxy(0,1);
+            sprintf(text2,"Rychlost = %3u %%",rychlost);
             lcd_puts(text2);
         }
         if(milis() - timeB > 1500){
             if (teplota_1_cast < 25){
-                TIM2_SetCompare1(0);
+                FAN_OFF;
+                dcl = 0;
                 rychlost = 0;
             }
             if(teplota_1_cast > 25){
-                TIM2_SetCompare1(128);
+                FAN_ON;
+                dcl = 128;
                 rychlost = 20;
             }
             if(teplota_1_cast > 30){
-                TIM2_SetCompare1(256);
+                dcl =256;
                 rychlost = 40;
             }
             if(teplota_1_cast > 35){
-                TIM2_SetCompare1(320);
+                dcl = 320;
                 rychlost = 50;
             }
             if(teplota_1_cast > 40){
-                TIM2_SetCompare1(384);
+                dcl = 384;
                 rychlost = 60;
             }
             if(teplota_1_cast > 45){
-                TIM2_SetCompare1(448);
+                dcl = 448;
                 rychlost = 70;
             }
             if(teplota_1_cast > 50){
-                TIM2_SetCompare1(512);
+                dcl = 512;
                 rychlost = 80;
             }
             if(teplota_1_cast > 55){
-                TIM2_SetCompare1(576);
+                dcl = 576;
                 rychlost = 90;
             }
             if(teplota_1_cast > 60){
-                TIM2_SetCompare1(639);
+                dcl = 639;
                 rychlost = 100;
             }
+            TIM2_SetCompare1(dcl);
+            printf("DCL = %3u \n \r",dcl);
             timeB=milis();
         }
     }
